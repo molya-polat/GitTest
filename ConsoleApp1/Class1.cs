47efdb629321d0e318bf33e2622eb033434165f0
Moldir Polat <moldir.polat@yandex.com>
2021-01-22 22:37:42 +0800

    refactored add method

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -4,11 +4,8 @@ public class Class1
 {
 	public Class1()
 	{
-		public int Add() {
-			int x1 = 1;
-			int x2 = 2;
-			int sum = x1 +x2;
-			return sum;
+		public int Add(int x1, int x2) {			
+			return x1+x2;
 		}
 	}
 }
